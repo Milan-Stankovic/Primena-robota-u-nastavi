786cc512cf7f6b74d3c2161eb7c07b7e620a35be
ilijagrbic <ilijagrb.ns@gmail.com>
2018-12-05 13:32:40 +0100

    Ispravljen kod po dogovoru od proslog puta (rekurzija samo stavlja u listu rezultate, pa treba probati da robota pomeramo na osnovu direkcija iz liste)

--- maze.js
+++ maze.js
@@ -1,67 +1,158 @@
-
 var myMaze = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-    [3, 1, 1, 0, 1, 0, 0, 1, 1, 1],
-    [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1, 1, 1, 0],
-    [0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+				[3, 1, 1, 0, 1, 0, 0, 1, 1, 1],
+				[0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+				[0, 1, 1, 0, 1, 0, 1, 1, 1, 0],
+				[0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+				[0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+			];
 
+			
+			
 
-function setUp(maze){
-	for(var i=0; i<maze.length; i++){
-		for(var j=0; j<maze[0].length; j++){
-			if(maze[i][j]==3){
-				return [i,j];
+			
+
+			function traverse(maze,column, row) {
+				
+				
+
+				if(maze[column][row] == 2) {
+					this.solved = true;
+					
+					moveTo(column, row, maze[column][row]);				
+				} else if((maze[column][row] == 1 || maze[column][row] == 3 )&& this.solved==false) {
+					maze[column][row] = 9;
+					
+					moveTo(column, row, maze[column][row]);
+					
+					if(column < maze.length - 1) {
+						traverse(maze, column + 1, row);
+					}
+					if(row < maze[column].length - 1) {
+						traverse(maze, column, row + 1);
+					}
+					if(column > 0) {
+						traverse(maze, column - 1, row);
+					}
+					if(row > 0) {
+						traverse(maze, column, row - 1);
+					}
+				}
 			}
-		}	
-	}
-}
-
-
-function traverse(maze,column, row) {
-        if(maze[column][row] == 2) {
-            console.log("We solved the maze at (" + column + ", " + row + ")");
-			console.log(maze);
 			
-        } else if(maze[column][row] == 1) {
+			function moveTo(column, row, field){
+				if(field==2) this.solved = true;
+				
+				var dir = findDirection(lastCol, lastRow, column, row);
+				if(dir!=-1){
+					this.directions.push(dir);
+					this.result.push(dir);
+				}
+				else{
+					var temp = findBackwardPass(lastCol, lastRow, column, row, this.directions);
+					for(var i = 0;i<temp.length;i++){
+						this.result.push(temp[i]);
+					}
+				}
+				this.lastCol = column;
+				this.lastRow = row;
+			}
 			
-            console.log("At valid position (" + column + ", " + row + ")");
-            maze[column][row] = 9;
-            if(column < maze.length - 1) {
-                traverse(column + 1, row);
-            }
-            if(row < maze[column].length - 1) {
-                traverse(column, row + 1);
-            }
-            if(column > 0) {
-                traverse(column - 1, row);
-            }
-            if(row > 0) {
-                traverse(column, row - 1);
-            }
-        }
-    }
+			function findBackwardPass(curCol, curRow, targetCol, targetRow, d){
+				var tempCol = curCol;
+				var tempRow = curRow;
+				
+				var retVal = [];
+				
+				var c=findDirection(tempCol, tempRow, targetCol, targetRow);
+				i=10;
+				while(c==-1&&(i--)){		
+					var next = negation(d.pop());
+					//console.log("Curent situation:"+c+"("+ tempCol + ", " + tempRow + ")+trying to do:"+next);
+					var novo = updatePosition(tempCol, tempRow, next);
+					tempCol=novo[0];
+					tempRow=novo[1];
+					retVal.push(next);
+					c=findDirection(tempCol, tempRow, targetCol, targetRow);
+				}
+				retVal.push(c);
+				d.push(c);
+				return retVal;
+			}
+			
+			function updatePosition(col, row, c){
+				if(c=='R') row++;
+				else if(c=='L') row--;
+				else if(c=='D') col++;
+				else if(c=='U') col--;
+				else return -1;
+				
+				return [col,row];
+			
+			}
+			
+			function negation(c){
+				if(c=='R') return 'L';
+				else if(c=='L') return 'R';
+				else if(c=='D') return 'U';
+				else if(c=='U') return 'D';
+				else return -1;
+			}
+			
+			function findDirection(curCol, curRow, targetCol, targetRow){
+				if(curCol==targetCol && curRow - targetRow == 1){
+					return 'L';
+				}				
+				else if(curCol==targetCol && curRow - targetRow == -1){
+					return 'R';
+				}
+				else if(curCol - targetCol == 1 && curRow == targetRow){
+					return 'U';
+				}
+				else if(curCol - targetCol == -1 && curRow == targetRow){
+					return 'D';
+				}
+				else{
+					return -1;
+				}
+			}
+			
+			function setUp(maze){
+				for(var i=0; i<maze.length; i++){
+					for(var j=0; j<maze[0].length; j++){
+						if(maze[i][j]==3){
+							return [i,j];
+						}
+					}	
+				}
+			}
+
+			
+			function MazeSolver(maze) {
+
+				this.maze = maze;
+				this.directions = [];//samo konacan put koji treba proci
+				this.result = [];//ukupan put sa svim vracanjima u nazad itd.
+				this.solved = false;
+				
+				var temp =setUp(maze);
+				
+				this.currentColumn = temp[0];
+				this.currentRow = temp[1];
+				
+				this.lastCol = temp[0];//pretpostavljam da pocinjemo od leve ivice
+				this.lastRow = temp[1]-1;//pretpostavljam da pocinjemo od leve ivice
+				
+				traverse(maze,currentColumn,currentRow);
+				//console.log(result);
+				//umesto ovog console log treba proci i slati robotu uputstva
+
+			}
 
 
-function MazeSolver(maze) {
-
-    this.maze = maze;
-	
-	var temp =setUp(maze);
-	
-	this.currentColumn = temp[0];
-	this.currentRow = temp[1];
-	traverse(maze,currentColumn,currentRow);
-	
-
-}
-
-
-MazeSolver(myMaze);
-
+			MazeSolver(myMaze);
+		
