38ad9b7dc4eb080321ff18b6d2f28adfcadcba0e
Milan Stankovic <milans995@gmail.com>
2019-01-04 21:05:18 +0100

    Git gud
    Git gud

--- maze.js
+++ maze.js
@@ -1,26 +1,213 @@
-var myMaze = [
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-				[3, 1, 1, 0, 1, 0, 0, 1, 1, 1],
-				[0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-				[0, 1, 1, 0, 1, 0, 1, 1, 1, 0],
-				[0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
-				[0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+/*async function startProgram() {
+	// Write code here
+	
+	
+	await roll(180, 15, 1);
+	await speak("Grba pederu");
+	await stopRoll;
+	await roll(90,15,1);
+}
+*/
+async function startProgram() {
+	setStabilization(true);
+	
+	
+	
+	//await leftMotorPwm();
+	
+	/*await rawMotor(60, 60, 2);
+	await stopRoll;
+	await setHeading(0);
+	await stopRoll;
+	await rawMotor(60, 60, 2);
+	await stopRoll;
+	await setHeading(90);
+	await stopRoll;
+	await rawMotor(60, 60, 2);
+	await stopRoll;
+	await setHeading(180);
+	await stopRoll;
+	await rawMotor(60, 60, 2);
+	await stopRoll;*/
+	//setStabilization(true);
+	MazeSolver(myMaze);
+	//await delay(2);
+	
+	//await speak(Math.round(getDistance()).toString());
+	//
+	
+	
+}
+	var myMaze = [
+				[1, 1, 1, 1, 1, 0, 0, 0, 1, 0,0],
+				[1, 0, 0, 0, 1, 1, 1, 0, 1, 1,1],
+				[1, 1, 0, 0, 0, 0, 1, 0, 0, 0,1],
+				[0, 1, 1, 1, 0, 0, 1, 0, 0, 0,1],
+				[0, 1, 0, 1, 0, 0, 1, 1, 1, 1,1],
+				[0, 1, 0, 1, 1, 0, 1, 0, 0, 0,0],
+				[0, 1, 0, 1, 0, 0, 1, 0, 0, 0,0],
+				[3, 1, 0, 0, 0, 0, 1, 1, 1, 1,2]
+				
 			];
 
-			
-			
+	var currentDistance = 0;
+	var lastDistance =0;
+	var maxDistance =0;
+	var lastMovement =-1;
 
-			
 
-			function traverse(maze,column, row) {
+	var speed = 100;//55
+	var time = 0.18;//
+	var right = 180;
+	var left= 0;
+	var up= 90;
+	var down = 270;
+
+
+
+
+			async function MazeSolver(maze) {
+				
+				this.maze = maze;
+				this.directions = [];//samo konacan put koji treba proci
+				this.result = [];//ukupan put sa svim vracanjima u nazad itd.
+				this.solved = false;
+				
+				var domePos;
+				var temp =setUp(maze);
+	
+				
+				this.currentColumn = temp[0];
+				this.currentRow = temp[1];
 				
 				
+				
+				this.lastCol = temp[0];//pretpostavljam da pocinjemo od leve ivice
+				this.lastRow = temp[1]-1;//pretpostavljam da pocinjemo od leve ivice
+				
+				traverse(maze,currentColumn,currentRow);
+				
+				for(var i=1;i<result.length;i++){
+					//await delay(1);
+					
+					await spin(360, 1);
+					
+					await stopRoll();
+					await delay(0.5);
+					//await speak(Math.round(getDistance()).toString());
+					var realTime = time;
+					if(result[i]==='R'){
+						await rotation(right);
+						//var d1 = getDistance();
+						//await roll(right, speed, (time*8.5/7));
+						
+						//var d2 = getDistance();
+						//await speak(Math.round(d2-d1).toString());
+						
+						realTime = (time*8.5)/7;
+						await stopRoll();
+						
+						//await setDomePosition(0);
+						await speak("Right");
+						
+					}
+					else if(result[i]=='L'){
+						
+						await rotation(left);
+						//var d1 = getDistance();
+						//await roll(left, speed,(time*8.5/7));
+						//var d2 = getDistance();
+						//await speak(Math.round(d2-d1).toString());
+						
+						realTime = (time*8.5)/7;
+						await stopRoll();
+						await speak("Left");
+					}
+					else if(result[i]=='U'){
+						
+						await rotation(up);
+						//var d1 = getDistance();
+						//await roll(up, speed, time);
+						
+						//var d2 = getDistance();
+						//await speak(Math.round(d2-d1).toString());
+						await stopRoll();
+						await speak("Up");
+					}
+					else if(result[i]=='D'){
+						
+						await rotation(down);
+						//var d1 = getDistance();
+						//await roll(down, speed, time);
+						//var d2 = getDistance();
+						//await speak(Math.round(d2-d1).toString());
+						await stopRoll();
+						await speak("Down");
+					}
+					
+					
+					
+					var d1 = getDistance();
+					await rawMotor(-speed,-speed,realTime);
+					var d2 = getDistance();
+					
+					//await stopRoll();
+					//await speak(Math.round(d2-d1).toString());
+				}
+				
+				//console.log(result);
+				//umesto ovog console log treba proci i slati robotu uputstva
 
+			}
+			
+			
+			
+
+
+				
+
+	async function rotation(newMovement){
+		
+		//await speak("Rotation");
+		
+		if(lastMovement != -1){
+			
+			//await speak("Second time");
+			
+			var nextAngle = newMovement - lastMovement;
+			
+			//await speak ("Pitch : " + getOrientation().pitch.toString());
+			//await speak ("Roll : " + getOrientation().roll.toString());
+			
+			
+			
+			
+			await spin(nextAngle, 0.7);
+			//await delay(1);
+			//await set
+			
+			//await speak("GIT GUD");
+			//await stopRoll();
+			lastMovement = newMovement;
+			//await speak("GIT GUD 2");
+		}else{
+			lastMovement = newMovement;
+		}
+	
+	}
+
+			
+
+		async function traverse(maze,column, row) {
+				
+				
+				
 				if(maze[column][row] == 2) {
+					
+					await rawMotor(-speed,-speed,realTime);
+					await rawMotor(-speed,-speed,realTime);
+					await rawMotor(-speed,-speed,realTime);
+					
 					this.solved = true;
 					
 					moveTo(column, row, maze[column][row]);				
@@ -132,27 +319,3 @@ var myMaze = [
 			}
 
 			
-			function MazeSolver(maze) {
-
-				this.maze = maze;
-				this.directions = [];//samo konacan put koji treba proci
-				this.result = [];//ukupan put sa svim vracanjima u nazad itd.
-				this.solved = false;
-				
-				var temp =setUp(maze);
-				
-				this.currentColumn = temp[0];
-				this.currentRow = temp[1];
-				
-				this.lastCol = temp[0];//pretpostavljam da pocinjemo od leve ivice
-				this.lastRow = temp[1]-1;//pretpostavljam da pocinjemo od leve ivice
-				
-				traverse(maze,currentColumn,currentRow);
-				//console.log(result);
-				//umesto ovog console log treba proci i slati robotu uputstva
-
-			}
-
-
-			MazeSolver(myMaze);
-		
